da7460e06504af1b066befefae4061a093751a1f
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-15 18:19:57 -0300

    feat: create product

--- src/app/routes/product.routes.ts
+++ src/app/routes/product.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { listProducts } from '../useCases/listProducts';
+import { createProduct } from '../useCases/createProduct';
+import { upload } from '../../config/multer';
 
 const productRouter = Router();
 
 productRouter.get('/', listProducts);
+productRouter.post('/', upload.single('image'), createProduct);
 
 export default productRouter;
---
+++ src/app/useCases/createProduct.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { Product } from '../models/Product';
+
+export async function createProduct(request: Request, response: Response) {
+  const imageReceived = request.file;
+  const { name, description, price, ingredients, category } = request.body;
+  const product = await Product.create({
+    name,
+    description,
+    imagePath: imageReceived?.filename,
+    price: Number(price),
+    ingredients: JSON.parse(ingredients),
+    category,
+  });
+  return response.status(201).json(product);
+}
