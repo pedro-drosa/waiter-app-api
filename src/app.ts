e6efef042a93d752be246731d6f085465ac017f1
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-19 14:30:23 -0300

    refactor: change http server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import http from 'node:http';
 import connectToDatabase from './database/mongo';
 import routes from './app/routes';
 import enableCors from './middlewares/enableCors';
@@ -6,9 +7,10 @@ import { UPLOADS_PATH } from './config/multer';
 
 connectToDatabase();
 const app = express();
+const server = http.createServer(app);
 app.use(enableCors);
 app.use('/uploads', express.static(UPLOADS_PATH));
 app.use(express.json());
 app.use(routes);
 
-export default app;
+export { server };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import app from './app';
+import { server } from './app';
 
 const APP_BASE_URL = 'http://localhost';
 const APP_PORT = 5000;
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log(`🚀 Server is running on: ${APP_BASE_URL}:${APP_PORT}`);
 });
