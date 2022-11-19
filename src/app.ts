53f152d1390d4c91c8dff880623c8d0fedf77f81
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-19 15:24:33 -0300

    feat(createOrder.ts): send order information with websocket

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import http from 'node:http';
+import { Server } from 'socket.io';
 import connectToDatabase from './database/mongo';
 import routes from './app/routes';
 import enableCors from './middlewares/enableCors';
@@ -8,9 +9,10 @@ import { UPLOADS_PATH } from './config/multer';
 connectToDatabase();
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 app.use(enableCors);
 app.use('/uploads', express.static(UPLOADS_PATH));
 app.use(express.json());
 app.use(routes);
 
-export { server };
+export { server, io };
--- src/app/useCases/createOrder.ts
+++ src/app/useCases/createOrder.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import { Order } from '../models/Order';
+import { io } from '../../app';
 
 export async function createOrder(request: Request, response: Response) {
   const { table, products } = request.body;
-  const order = await Order.create({
-    table,
-    products,
-  });
+  const order = await Order.create({ table, products });
+  const orderDetails = await order.populate('products.product');
+  io.emit('order@new', orderDetails);
   response.json(order);
 }
